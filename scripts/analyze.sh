909d70b0459df735da0ea7c67a1e02a8aaa4971d
izumin5210 <m@izum.in>
2022-07-17 20:57:59 +0900

    fix analyze script

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-
 timestamp=$(date +'%H%M%S')
-LOG_DIR="/home/${USER}/log/${timestap}"
+LOG_DIR="/home/${USER}/log/${timestamp}"
 
 mkdir -p $LOG_DIR
 
-sudo cp /var/log/nginx/access.log ${LOG_DIR}/nginx.access.log
-sudo cp /var/log/mysql/mysql-slow.log ${LOG_DIR}/mysql.slow.log
+sudo mv /var/log/nginx/access.log ${LOG_DIR}/nginx.access.log
+sudo mv /var/log/mysql/mysql-slow.log ${LOG_DIR}/mysql.slow.log
 
 sudo systemctl reload nginx
 sudo systemctl restart mysql
