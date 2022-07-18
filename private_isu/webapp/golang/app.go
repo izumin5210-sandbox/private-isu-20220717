9f7699717dac801217df420fc2d44ed1517d589f
izumin5210 <m@izum.in>
2022-07-18 13:26:51 +0900

    Check user del_flg on fetch posts

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -416,7 +416,7 @@ func getIndex(w http.ResponseWriter, r *http.Request) {
 
 	results := []Post{}
 
-	err := db.Select(&results, "SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` ORDER BY `created_at` DESC LIMIT ?", postsPerPage)
+	err := db.Select(&results, "SELECT `posts.id`, `posts.user_id`, `posts.body`, `posts.mime`, `posts.created_at` FROM `posts` INNER JOIN `users` ON `posts`.`user_id` = `users.id` WHERE `users`.`del_flg` = 0 ORDER BY `created_at` DESC LIMIT ?", postsPerPage)
 	if err != nil {
 		log.Print(err)
 		return
